972c93fb9f84e51a4a7311a1cdaf93c094581a3e
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-10-30 09:34:52 -0700

    Remove unnecessary comments

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 
       if (!isNaN(lastToken) || lastToken === '%') {
         let symbol;
-        
+
         switch (buttonID) {
           case 'add':
             symbol = '+';
@@ -104,8 +104,6 @@ $(document).ready(function() {
       let before = tokens.slice(0, negatePos - 1);
       let after = tokens.slice(negatePos + 2);
       tokens = before.concat([computed]).concat(after);
-
-      console.log( tokens );
       
       var negatePos = tokens.indexOf('~');
     }
@@ -151,87 +149,3 @@ $(document).ready(function() {
   }
 
 });
-
-/*
-  let $number = $('#number');
-
-  // the idea is that whenever you press an operator, we evaluate
-  // currentOperator(previousNumber, currentNumber), e.g. (3, +, 4) => 3+4
-  // this is then stored to previousNumber
-  // 
-  let previousNumber, currentOperator, currentNumber;
-
-  function buttonPressed(event) {
-    console.log(event.target.id); // DEBUG
-    let buttonID = event.target.id;
-    let num = +$number.text();
-
-    if (!isNaN(+buttonID) || buttonID === 'decimal') {
-      // numbers
-      handleNumber(buttonID);
-
-    } else if (buttonID === "clear") {
-      $number.text('0');
-      previousNumber = undefined;
-      currentOperator = undefined;
-    } else if (buttonID === "sign") {
-      $number.text( -num );
-    } else if (buttonID === "percent") {
-      $number.text( 0.01 * num );
-    } else if (buttonID === "equals") {
-      // apply();
-
-    } else {
-      // operators
-      handleOperator(buttonID, previousNumber, num);
-      previousNumber = num;
-      currentOperator = buttonID;
-    }
-  }
-
-  function handleNumber(num) {
-    console.log('num:', num);
-
-    let currentValue = $number.text();
-    var newValue;
-
-    if (num === 'decimal' && currentValue.indexOf('.') === -1) {
-      newValue = currentValue + '.';
-    } else if (num !== 'decimal') {
-      newValue = (currentValue === '0') ? num : currentValue + '' + num;
-    }
-
-    $number.text( newValue );
-  }
-
-  // e.g. (divide, a, b) => a / b
-  function handleOperator(op, a, b) {
-    console.log(op, a, b);
-
-    if (a !== undefined) {
-
-      switch (op) {
-        case add:
-          console.log(op, a, b);
-          $number.text(+a + +b);
-          break;
-        default:
-          // nothing
-      }
-
-      previousNumber = undefined;
-    }
-
-  }
-
-  // function apply() {
-  //   if (callStack.length >= 3) {
-  //     console.log(callStack.pop());
-  //     console.log(callStack.pop());
-  //     console.log(callStack.pop());
-  //     console.log(callStack);
-  //   }
-  // }
-
-});
-*/
